855fd908a24e12a08f1d7ba34e85fb663aeb943d
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-07 16:30:59 -0600

    Added function FlipACoin

--- FlipACoin/Program.cs
+++ FlipACoin/Program.cs
@@ -8,8 +8,35 @@ namespace FlipACoin
 {
     class Program
     {
+        static Random rng = new Random();
+        static int headsCounter = 0;
+        static int tailsCounter = 0;
+
         static void Main(string[] args)
         {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Flipped: " + FlipACoin());
+            }
+            Console.WriteLine("#Heads: " + headsCounter);
+            Console.WriteLine("#Tails: " + tailsCounter);
+            Console.ReadKey();
+        }
+
+        public static string FlipACoin()
+        {
+            if (rng.Next(2) == 0)
+            {
+                // heads
+                headsCounter++;
+                return "Heads";
+            }
+            else
+            {
+                //tails
+                tailsCounter++;
+                return "Tails";
+            }
         }
     }
 }
